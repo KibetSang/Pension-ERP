ff688b8d2b1aedd151d44e33dce18e2fd6db1e92
Kibet Sang <kibetsang95@gmail.com>
2026-01-22 16:36:28 +0300

    feat: Add Pension Scheme and Fund DocTypes

---
+++ pension_erp/pension_erp/doctype/pension_fund/pension_fund.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Kibet Sang and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Pension Fund", {
+// 	refresh(frm) {
+
+// 	},
+// });
